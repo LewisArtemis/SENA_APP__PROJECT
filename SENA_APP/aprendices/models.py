76cf32cc974a964691b41abc152c3a5a55ea7bd0
LewisArtemis <oviedoninol@gmail.com>
2025-07-29 10:08:38 -0500

    creacion de los modelos

--- SENA_APP/aprendices/models.py
+++ SENA_APP/aprendices/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Aprendiz(models.Model):
+    documento_identidad = models.CharField(max_length=30)
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    telefono = models.CharField(max_length=30)
+    correo = models.EmailField()
+    fecha_nacimiento = models.DateField()
+    ciudad = models.CharField(max_length=30)
+    programa = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return f"{self.nombre} {self.apellido} {self.documento_identidad}"
+    
