d1a4fe7560c30ec71459fb69bb063edb25f590fe
LewisArtemis <oviedoninol@gmail.com>
2025-08-01 11:15:43 -0500

    creacion de nuevos modelos

--- SENA_APP/aprendices/models.py
+++ SENA_APP/aprendices/models.py
@@ -10,7 +10,7 @@ class Aprendiz(models.Model):
     correo = models.EmailField()
     fecha_nacimiento = models.DateField()
     ciudad = models.CharField(max_length=30)
-    programa = models.CharField(max_length=30)
+    #programa = models.CharField(max_length=30)
     
     def __str__(self):
         return f"{self.nombre} {self.apellido} {self.documento_identidad}"
